2a46d8cccabc5fcc9140d800c6d31a707a370fbd
Chris Gatcombe <chris@gatcombe.com>
2021-11-26 10:55:48 -0500

    Added build batch file; Added VenuSq device to manifest. Make this project build for VenuSq device. Updated Makefile (not sure this is actually used).

---
+++ garmin-connect-seed-master/monkeyc_build.bat
@@ -0,0 +1,13 @@
+@echo off
+set monkeyc=C:\Users\chris\AppData\Roaming\Garmin\ConnectIQ\Sdks\connectiq-sdk-win-4.0.6-2021-10-06-af9b9d6e2\bin
+set device=venusq
+set prg=GarminConnectSeedMaster.prg
+
+echo ConnectIQ...
+call %monkeyc%\connectiq
+timeout 10
+echo MonkeyC...
+call %monkeyc%\monkeyc -d %device% -f monkey.jungle -o %prg% -y .vscode\developer_key
+rem pause
+echo MonkeyDo...
+call %monkeyc%\monkeydo %prg% %device%
--- garmin-connect-seed-master/properties.mk
+++ garmin-connect-seed-master/properties.mk
@@ -1,4 +1,5 @@
-DEVICE = fenix5s
-SDK_HOME = /Users/daniel.siwiec/connectiq-sdk-mac-3.0.4
-DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
-PRIVATE_KEY = /Users/daniel.siwiec/.ssh/id_rsa_garmin.der
+#DEVICE = venusq
+DEVICE = vivoactive_hr
+SDK_HOME = C:\Users\chris\AppData\Roaming\Garmin\ConnectIQ\Sdks\connectiq-sdk-win-4.0.6-2021-10-06-af9b9d6e2
+#DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
+PRIVATE_KEY = c:\Chris\Garmin\Garmin Venu Sq watch faces\garmin-connect-seed-master\.vscode\developer_key
